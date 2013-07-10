317ae5b7011c30a2346953632ae617e953a2aab8
Björn Steffen <bjoern.steffen@inf.ethz.ch>
2013-07-10 13:25:57 +0200

    utf-8 überall

--- init.el
+++ init.el
@@ -179,7 +179,13 @@ Missing packages are installed automatically."
 (setq standard-indent 2)
 
 
+(setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
+(set-language-environment 'utf-8)
+;(set-keyboard-coding-system 'utf-8-mac) ; For old Carbon emacs on OS X only
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
 
 ;; ido-mode is like magic pixie dust!
 (require 'ido-ubiquitous)
